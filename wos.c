7d797d2d434fda089a796a06f77c669b8c7b09b9
wxdao <waxiadao@gmail.com>
2018-02-03 22:42:59 +0800

    format code

--- wos.c
+++ wos.c
@@ -2,25 +2,25 @@
 
 #include "stm32f10x.h"
 
-#define SYSCALL_OS_START     0
-#define SYSCALL_TASK_CREATE  1
-#define SYSCALL_TASK_SLEEP   2
-#define SYSCALL_TASK_EXIT    3
-#define SYSCALL_TASK_KILL    4
+#define SYSCALL_OS_START 0
+#define SYSCALL_TASK_CREATE 1
+#define SYSCALL_TASK_SLEEP 2
+#define SYSCALL_TASK_EXIT 3
+#define SYSCALL_TASK_KILL 4
 #define SYSCALL_QUEUE_CREATE 5
-#define SYSCALL_QUEUE_PUSH   6
-#define SYSCALL_QUEUE_PULL   7
-#define SYSCALL_QUEUE_CLOSE  8
+#define SYSCALL_QUEUE_PUSH 6
+#define SYSCALL_QUEUE_PULL 7
+#define SYSCALL_QUEUE_CLOSE 8
 
-#define TASK_STATE_FREE      0
-#define TASK_STATE_RUNNING   1
-#define TASK_STATE_READY     2
-#define TASK_STATE_SLEEPING  3
+#define TASK_STATE_FREE 0
+#define TASK_STATE_RUNNING 1
+#define TASK_STATE_READY 2
+#define TASK_STATE_SLEEPING 3
 #define TASK_WAITING_TO_PUSH 5
 #define TASK_WAITING_TO_PULL 6
 
-#define QUEUE_STATE_FREE     0
-#define QUEUE_STATE_NORMAL   1
+#define QUEUE_STATE_FREE 0
+#define QUEUE_STATE_NORMAL 1
 
 // contexts defs
 
@@ -117,55 +117,48 @@ static queue_control_block_t qcbs[MAX_QUEUES] = {0};
 static void *memset(void *s, int c, uint32_t n) {
   const unsigned char uc = c;
   unsigned char *su;
-  for(su = s; 0 < n; ++su, --n)
+  for (su = s; 0 < n; ++su, --n)
     *su = uc;
   return s;
 }
 
 static void *memmove(void *dst, const void *src, uint32_t count) {
-    char *tmpdst = (char *)dst;
-    char *tmpsrc = (char *)src;
-    if (tmpdst <= tmpsrc || tmpdst >= tmpsrc + count){
-      while(count--) {
-          *tmpdst++ = *tmpsrc++; 
-      }
+  char *tmpdst = (char *)dst;
+  char *tmpsrc = (char *)src;
+  if (tmpdst <= tmpsrc || tmpdst >= tmpsrc + count) {
+    while (count--) {
+      *tmpdst++ = *tmpsrc++;
     }
-    else {
-      tmpdst = tmpdst + count - 1;
-      tmpsrc = tmpsrc + count - 1;
-      while(count--) {
-        *tmpdst-- = *tmpsrc--;
-      }
+  } else {
+    tmpdst = tmpdst + count - 1;
+    tmpsrc = tmpsrc + count - 1;
+    while (count--) {
+      *tmpdst-- = *tmpsrc--;
     }
-    return dst; 
+  }
+  return dst;
 }
 
 // helper inline func to set PendSV exception pending
-static inline void set_pendsv(void) {
-  SCB->ICSR = SCB_ICSR_PENDSVSET;
-}
+static inline void set_pendsv(void) { SCB->ICSR = SCB_ICSR_PENDSVSET; }
 
-// yield, which simply set PendSV pending so PendSV_Handler will be invoked upon return
-static void os_yield(void) {
-  set_pendsv();
-}
+// yield, which simply set PendSV pending so PendSV_Handler will be invoked upon
+// return
+static void os_yield(void) { set_pendsv(); }
 
 // 'syscall' wrapper
-static inline uint32_t syscall(uint32_t r0, uint32_t r1, uint32_t r2, uint32_t r3) {
+static inline uint32_t syscall(uint32_t r0, uint32_t r1, uint32_t r2,
+                               uint32_t r3) {
   uint32_t ret;
-  __asm__ volatile(
-    R"(
-    mov r0, %1
-    mov r1, %2
-    mov r2, %3
-    mov r3, %4
-    svc #0x80
-    mov %0, r0
-    )"
-    :"=r"(ret)
-    :"r"(r0), "r"(r1), "r"(r2), "r"(r3)
-    :"r0", "r1", "r2", "r3"
-  );
+  __asm__ volatile("mov r0, %1 \n\t"
+                   "mov r1, %2 \n\t"
+                   "mov r2, %3 \n\t"
+                   "mov r3, %4 \n\t"
+                   "svc #0x80  \n\t"
+                   "mov %0, r0 \n\t"
+                   : "=r"(ret)
+                   : "r"(r0), "r"(r1), "r"(r2), "r"(r3)
+                   : "r0", "r1", "r2", "r3");
   return ret;
 }
 
@@ -194,7 +187,7 @@ void leave_critical(void) {
 // this task is run when no other tasks are ready
 static void _task_suspending(void *arg) {
   for (;;) {
-    __asm__ ("wfe");
+    __asm__("wfe");
   }
 }
 
@@ -207,7 +200,8 @@ static int32_t do_task_create(task_create_param_t *param) {
   enter_critical();
   // find memory for task stack
   // this algorithm is ugly...
-  uint32_t next_task_stack_base = (uint32_t)(tasks_stack_pool + sizeof(tasks_stack_pool)/8) - 1;
+  uint32_t next_task_stack_base =
+      (uint32_t)(tasks_stack_pool + sizeof(tasks_stack_pool) / 8) - 1;
   int found = 1;
   for (int i = -1; i < MAX_TASKS; i++) {
     if (i != -1 && tcbs[i].state == TASK_STATE_FREE) {
@@ -215,7 +209,8 @@ static int32_t do_task_create(task_create_param_t *param) {
     }
     uint32_t i_stack_base, i_stack_size;
     if (i == -1) {
-      i_stack_base = (uint32_t)(tasks_stack_pool + sizeof(tasks_stack_pool)/8) - 1;
+      i_stack_base =
+          (uint32_t)(tasks_stack_pool + sizeof(tasks_stack_pool) / 8) - 1;
       i_stack_size = 0;
     } else {
       i_stack_base = tcbs[i].stack_base;
@@ -242,7 +237,8 @@ static int32_t do_task_create(task_create_param_t *param) {
         highest_stack_base = j_stack_base;
       }
     }
-    if (highest_stack_base != 0 && highest_stack_base <= i_stack_base - i_stack_size - param->stack_size) {
+    if (highest_stack_base != 0 &&
+        highest_stack_base <= i_stack_base - i_stack_size - param->stack_size) {
       next_task_stack_base = i_stack_base - i_stack_size;
       found = 1;
       break;
@@ -255,12 +251,13 @@ static int32_t do_task_create(task_create_param_t *param) {
   }
 
   // build context
-  task_context_t *task_ctx = (task_context_t *)(next_task_stack_base - sizeof(task_context_t) + 1);
+  task_context_t *task_ctx =
+      (task_context_t *)(next_task_stack_base - sizeof(task_context_t) + 1);
   memset(task_ctx, 0, sizeof(task_context_t));
   task_ctx->int_ctx.xpsr = 0x01000000;
-  task_ctx->int_ctx.pc = (uint32_t)(uint32_t*)param->entry | 1;
-  task_ctx->int_ctx.lr = (uint32_t)(uint32_t*)task_exit | 1;
-  task_ctx->int_ctx.r0 = (uint32_t)(uint32_t*)param->arg;
+  task_ctx->int_ctx.pc = (uint32_t)(uint32_t *)param->entry | 1;
+  task_ctx->int_ctx.lr = (uint32_t)(uint32_t *)task_exit | 1;
+  task_ctx->int_ctx.r0 = (uint32_t)(uint32_t *)param->arg;
 
   // build tcb
   task_control_block_t *tcb = 0;
@@ -316,7 +313,8 @@ static int32_t do_queue_create(queue_create_param_t *param) {
   enter_critical();
   // find memory for queue data
   // ugly algorithm. could have been better
-  uint32_t next_queue_data_base = (uint32_t)(queues_data_pool + sizeof(queues_data_pool)) - 1;
+  uint32_t next_queue_data_base =
+      (uint32_t)(queues_data_pool + sizeof(queues_data_pool)) - 1;
   int found = 1;
   for (int i = -1; i < MAX_QUEUES; i++) {
     if (i != -1 && qcbs[i].state == QUEUE_STATE_FREE) {
@@ -351,7 +349,9 @@ static int32_t do_queue_create(queue_create_param_t *param) {
         highest_data_base = j_data_base;
       }
     }
-    if (highest_data_base != 0 && highest_data_base <= i_data_base - i_data_size - param->item_size*param->length) {
+    if (highest_data_base != 0 &&
+        highest_data_base <=
+            i_data_base - i_data_size - param->item_size * param->length) {
       next_queue_data_base = i_data_base - i_data_size;
       found = 1;
       break;
@@ -424,9 +424,15 @@ static void do_queue_tasks_pull(void) {
         }
       }
       // pull
-      memmove(pull_tcbs[highest_priority_task_i]->trans_item, (void *)(qcbs[i].data_base - qcbs[i].item_size + 1), qcbs[i].item_size);
+      memmove(pull_tcbs[highest_priority_task_i]->trans_item,
+              (void *)(qcbs[i].data_base - qcbs[i].item_size + 1),
+              qcbs[i].item_size);
       // adjust
-      memmove((void *)(qcbs[i].data_base - qcbs[i].item_size*(qcbs[i].count - 1) + 1), (void *)(qcbs[i].data_base - qcbs[i].item_size*qcbs[i].count + 1), qcbs[i].item_size*(qcbs[i].count - 1));
+      memmove(
+          (void *)(qcbs[i].data_base - qcbs[i].item_size * (qcbs[i].count - 1) +
+                   1),
+          (void *)(qcbs[i].data_base - qcbs[i].item_size * qcbs[i].count + 1),
+          qcbs[i].item_size * (qcbs[i].count - 1));
       pull_tcbs[highest_priority_task_i]->ctx->int_ctx.r0 = 0;
       pull_tcbs[highest_priority_task_i]->state = TASK_STATE_READY;
 
@@ -452,7 +458,8 @@ static void do_queue_tasks_push(void) {
       continue;
     }
     // push
-    memmove((void *)(qcb->data_base - qcb->item_size*(qcb->count+1) + 1), tcbs[i].trans_item, qcb->item_size);
+    memmove((void *)(qcb->data_base - qcb->item_size * (qcb->count + 1) + 1),
+            tcbs[i].trans_item, qcb->item_size);
     qcb->count++;
     tcbs[i].ctx->int_ctx.r0 = 0;
     tcbs[i].state = TASK_STATE_READY;
@@ -460,7 +467,8 @@ static void do_queue_tasks_push(void) {
   leave_critical();
 }
 
-// queue push and pull are similar, and the process to really push/pull is in do_os_sche
+// queue push and pull are similar, and the process to really push/pull is in
+// do_os_sche
 static int32_t do_queue_push(queue_trans_param_t *param) {
   enter_critical();
   if (qcbs[param->queue_id].state == QUEUE_STATE_FREE) {
@@ -499,7 +507,8 @@ static int32_t do_queue_close(uint16_t queue_id) {
   enter_critical();
   qcbs[queue_id].state = QUEUE_STATE_FREE;
   for (int i = 0; i < MAX_TASKS; i++) {
-    if (tcbs[i].state != TASK_WAITING_TO_PUSH && tcbs[i].state != TASK_WAITING_TO_PULL) {
+    if (tcbs[i].state != TASK_WAITING_TO_PUSH &&
+        tcbs[i].state != TASK_WAITING_TO_PULL) {
       continue;
     }
     if (tcbs[i].trans_queue_id != queue_id) {
@@ -535,18 +544,14 @@ static int32_t do_os_start(void) {
       register int r0 asm("r0") = (int)current_tcb->ctx;
       register int r1 asm("r1") = (int)&os_started;
       register int r2 asm("r2") = 1;
-      __asm__ volatile(
-        R"(
-        ldmia %0!, {r4-r11}
-        msr psp, %0
+      __asm__ volatile("ldmia %0!, {r4-r11} \n\t"
+                       "msr psp, %0         \n\t"
 
-        str %2, [%1]
-        ldr %0, =0xFFFFFFFD
-        bx %0
-        )"
-        :
-        :"r"(r0), "r"(r1), "r"(r2)
-      );
+                       "str %2, [%1]        \n\t"
+                       "ldr %0, =0xFFFFFFFD \n\t"
+                       "bx %0               \n\t"
+                       :
+                       : "r"(r0), "r"(r1), "r"(r2));
       // should not be here
       return 0;
     }
@@ -572,14 +577,10 @@ static void do_os_tick(void) {
 static void do_os_sche(void) {
   // save current task
   task_control_block_t *tcb = current_tcb;
-  __asm__ volatile(
-    R"(
-    mrs %0, psp
-    )"
-    :"=r"(tcb->ctx)
-  );
+  __asm__ volatile("mrs %0, psp" : "=r"(tcb->ctx));
 
-  int task_i = ((uint32_t)current_tcb - (uint32_t)tcbs)/sizeof(task_control_block_t);
+  int task_i =
+      ((uint32_t)current_tcb - (uint32_t)tcbs) / sizeof(task_control_block_t);
 
   enter_critical();
   // handling
@@ -592,13 +593,15 @@ static void do_os_sche(void) {
       if (tcbs[i].state == TASK_STATE_SLEEPING) {
         tcbs[i].state = TASK_STATE_READY;
       }
-      if (tcbs[i].state == TASK_WAITING_TO_PUSH || tcbs[i].state == TASK_WAITING_TO_PULL) {
+      if (tcbs[i].state == TASK_WAITING_TO_PUSH ||
+          tcbs[i].state == TASK_WAITING_TO_PULL) {
         // timeout
         tcbs[i].ctx->int_ctx.r0 = RET_QUEUE_TRANS_TIMEOUT;
         tcbs[i].state = TASK_STATE_READY;
       }
     }
-    if (tcbs[i].state != TASK_STATE_READY && tcbs[i].state != TASK_STATE_RUNNING) {
+    if (tcbs[i].state != TASK_STATE_READY &&
+        tcbs[i].state != TASK_STATE_RUNNING) {
       continue;
     }
     if (tcbs[i].priority < highest_priority) {
@@ -639,13 +642,7 @@ static void do_os_sche(void) {
   if (tcb->state == TASK_STATE_RUNNING) {
     tcb->state = TASK_STATE_READY;
   }
-  __asm__ volatile(
-    R"(
-    msr psp, %0
-    )"
-    :
-    :"r"(next_tcb->ctx)
-  );
+  __asm__ volatile("msr psp, %0" : : "r"(next_tcb->ctx));
   next_tcb->state = TASK_STATE_RUNNING;
   current_tcb = next_tcb;
   leave_critical();
@@ -679,7 +676,8 @@ int32_t queue_push_isr(uint16_t queue_id, void *item) {
     leave_critical();
     return RET_QUEUE_TRANS_FULL;
   }
-  memmove((void *)(qcb->data_base - qcb->item_size*(qcb->count+1) + 1), item, qcb->item_size);
+  memmove((void *)(qcb->data_base - qcb->item_size * (qcb->count + 1) + 1),
+          item, qcb->item_size);
   qcb->count++;
   do_queue_tasks_pull();
   leave_critical();
@@ -708,7 +706,9 @@ int32_t queue_pull_isr(uint16_t queue_id, void *item) {
   }
   memmove(item, (void *)(qcb->data_base - qcb->item_size + 1), qcb->item_size);
   // adjust
-  memmove((void *)(qcb->data_base - qcb->item_size*(qcb->count - 1) + 1), (void *)(qcb->data_base - qcb->item_size*qcb->count + 1), qcb->item_size*(qcb->count - 1));
+  memmove((void *)(qcb->data_base - qcb->item_size * (qcb->count - 1) + 1),
+          (void *)(qcb->data_base - qcb->item_size * qcb->count + 1),
+          qcb->item_size * (qcb->count - 1));
   qcb->count--;
   do_queue_tasks_push();
   leave_critical();
@@ -720,25 +720,18 @@ void queue_close(uint16_t queue_id) {
   syscall(SYSCALL_QUEUE_CLOSE, queue_id, 0, 0);
 }
 
-void queue_close_isr(uint16_t queue_id) {
-  do_queue_close(queue_id);
-}
+void queue_close_isr(uint16_t queue_id) { do_queue_close(queue_id); }
 
 // task methods (public methods)
 
-void task_exit(void) {
-  syscall(SYSCALL_TASK_EXIT, 0, 0, 0);
-}
+void task_exit(void) { syscall(SYSCALL_TASK_EXIT, 0, 0, 0); }
 
-void task_kill(uint16_t task_id) {
-  syscall(SYSCALL_TASK_KILL, task_id, 0, 0);
-}
+void task_kill(uint16_t task_id) { syscall(SYSCALL_TASK_KILL, task_id, 0, 0); }
 
-void task_sleep(uint32_t ms) {
-  syscall(SYSCALL_TASK_SLEEP, ms, 0, 0);
-}
+void task_sleep(uint32_t ms) { syscall(SYSCALL_TASK_SLEEP, ms, 0, 0); }
 
-int32_t task_create(task_func_t entry, void *arg, uint32_t stack_size, uint8_t priority) {
+int32_t task_create(task_func_t entry, void *arg, uint32_t stack_size,
+                    uint8_t priority) {
   volatile task_create_param_t param;
   param.entry = entry;
   param.arg = arg;
@@ -747,7 +740,8 @@ int32_t task_create(task_func_t entry, void *arg, uint32_t stack_size, uint8_t p
   return syscall(SYSCALL_TASK_CREATE, (int)&param, 0, 0);
 }
 
-int32_t task_create_isr(task_func_t entry, void *arg, uint32_t stack_size, uint8_t priority) {
+int32_t task_create_isr(task_func_t entry, void *arg, uint32_t stack_size,
+                        uint8_t priority) {
   task_create_param_t param;
   param.entry = entry;
   param.arg = arg;
@@ -758,9 +752,7 @@ int32_t task_create_isr(task_func_t entry, void *arg, uint32_t stack_size, uint8
 
 // os methods (public methods)
 
-int32_t os_start(void) {
-  return syscall(SYSCALL_OS_START, 0, 0, 0);
-}
+int32_t os_start(void) { return syscall(SYSCALL_OS_START, 0, 0, 0); }
 
 // handlers
 
@@ -771,90 +763,78 @@ void Post_SVC_Handler(interrupt_context_t *ctx) {
   }
 
   switch (ctx->r0) {
-    case SYSCALL_OS_START: {
-      ctx->r0 = do_os_start();
-      break;
-    }
-    case SYSCALL_TASK_CREATE: {
-      task_create_param_t *param = (task_create_param_t *)ctx->r1;
-      ctx->r0 = do_task_create(param);
-      break;
-    }
-    case SYSCALL_TASK_SLEEP: {
-      ctx->r0 = do_task_sleep(ctx->r1);
-      break;
-    }
-    case SYSCALL_TASK_EXIT: {
-      ctx->r0 = do_task_exit();
-      break;
-    }
-    case SYSCALL_TASK_KILL: {
-      ctx->r0 = do_task_kill(ctx->r1);
-      break;
-    }
-    case SYSCALL_QUEUE_CREATE: {
-      queue_create_param_t *param = (queue_create_param_t *)ctx->r1;
-      ctx->r0 = do_queue_create(param);
-      break;
-    }
-    case SYSCALL_QUEUE_PUSH: {
-      queue_trans_param_t *param = (queue_trans_param_t *)ctx->r1;
-      ctx->r0 = do_queue_push(param);
-      break;
-    }
-    case SYSCALL_QUEUE_PULL: {
-      queue_trans_param_t *param = (queue_trans_param_t *)ctx->r1;
-      ctx->r0 = do_queue_pull(param);
-      break;
-    }
-    case SYSCALL_QUEUE_CLOSE: {
-      ctx->r0 = do_queue_close(ctx->r1);
-      break;
-    }
+  case SYSCALL_OS_START: {
+    ctx->r0 = do_os_start();
+    break;
+  }
+  case SYSCALL_TASK_CREATE: {
+    task_create_param_t *param = (task_create_param_t *)ctx->r1;
+    ctx->r0 = do_task_create(param);
+    break;
+  }
+  case SYSCALL_TASK_SLEEP: {
+    ctx->r0 = do_task_sleep(ctx->r1);
+    break;
+  }
+  case SYSCALL_TASK_EXIT: {
+    ctx->r0 = do_task_exit();
+    break;
+  }
+  case SYSCALL_TASK_KILL: {
+    ctx->r0 = do_task_kill(ctx->r1);
+    break;
+  }
+  case SYSCALL_QUEUE_CREATE: {
+    queue_create_param_t *param = (queue_create_param_t *)ctx->r1;
+    ctx->r0 = do_queue_create(param);
+    break;
+  }
+  case SYSCALL_QUEUE_PUSH: {
+    queue_trans_param_t *param = (queue_trans_param_t *)ctx->r1;
+    ctx->r0 = do_queue_push(param);
+    break;
+  }
+  case SYSCALL_QUEUE_PULL: {
+    queue_trans_param_t *param = (queue_trans_param_t *)ctx->r1;
+    ctx->r0 = do_queue_pull(param);
+    break;
+  }
+  case SYSCALL_QUEUE_CLOSE: {
+    ctx->r0 = do_queue_close(ctx->r1);
+    break;
+  }
   }
 }
 
 // svc handler wrapping, written in asm to put context on argument
 __attribute__((naked)) void SVC_Handler(void) {
-  __asm__(
-    R"(
-    tst lr, #4
-    ite eq
-    mrseq r0, msp
-    mrsne r0, psp
+  __asm__("tst lr, #4          \n\t"
+          "ite eq              \n\t"
+          "mrseq r0, msp       \n\t"
+          "mrsne r0, psp       \n\t"
 
-    push {lr}
-    bl Post_SVC_Handler
-    pop {pc}
-    )"
-  );
+          "push {lr}           \n\t"
+          "bl Post_SVC_Handler \n\t"
+          "pop {pc}            \n\t");
 }
 
-void SysTick_Handler(void) {
-  do_os_tick();
-}
+void SysTick_Handler(void) { do_os_tick(); }
 
-void Post_PendSV_Handler(void) {
-  do_os_sche();
-}
+void Post_PendSV_Handler(void) { do_os_sche(); }
 
-// pendsv handler wrapping, written in asm to save task context on entry and to restore it(may be a new one) on exit
+// pendsv handler wrapping, written in asm to save task context on entry and to
+// restore it(may be a new one) on exit
 __attribute__((naked)) void PendSV_Handler(void) {
-  __asm__(
-    R"(
-    push {lr}
+  __asm__("push {lr}              \n\t"
+          "mrs lr, psp            \n\t"
+          "stmdb lr!, {r4-r11}    \n\t"
+          "msr psp, lr            \n\t"
 
-    mrs lr, psp
-    stmdb lr!, {r4-r11}
-    msr psp, lr
+          "bl Post_PendSV_Handler \n\t"
 
-    bl Post_PendSV_Handler
+          "mrs lr, psp            \n\t"
+          "ldmia lr!, {r4-r11}    \n\t"
+          "msr psp, lr            \n\t"
 
-    mrs lr, psp
-    ldmia lr!, {r4-r11}
-    msr psp, lr
-
-    pop {pc}
-    )"
-  );
+          "pop {pc}               \n\t");
 }
--- wos.h
+++ wos.h
@@ -6,10 +6,10 @@
 // configs
 
 #define TASKS_STACK_POOL_SIZE 0x2000
-#define MAX_TASKS             10
+#define MAX_TASKS 10
 
 #define QUEUES_DATA_POOL_SIZE 0x400
-#define MAX_QUEUES            5
+#define MAX_QUEUES 5
 
 // defs
 
@@ -17,21 +17,21 @@
 
 // return codes
 
-#define RET_OS_START_ALREADY_STARTED             -1
-#define RET_OS_START_NO_TASK                     -2
-#define RET_OS_START_CANNOT_CREATE_TASKS         -3
+#define RET_OS_START_ALREADY_STARTED -1
+#define RET_OS_START_NO_TASK -2
+#define RET_OS_START_CANNOT_CREATE_TASKS -3
 
 #define RET_TASK_CREATE_INSUFFICIENT_STACK_SPACE -1
-#define RET_TASK_CREATE_TOO_MANY_TASKS           -2
-#define RET_TASK_CREATE_NOT_ALIGNED              -3
+#define RET_TASK_CREATE_TOO_MANY_TASKS -2
+#define RET_TASK_CREATE_NOT_ALIGNED -3
 
 #define RET_QUEUE_CREATE_INSUFFICIENT_DATA_SPACE -1
-#define RET_QUEUE_CREATE_TOO_MANY_QUEUES         -2
+#define RET_QUEUE_CREATE_TOO_MANY_QUEUES -2
 
-#define RET_QUEUE_TRANS_INVALID                  -1
-#define RET_QUEUE_TRANS_FULL                     -2
-#define RET_QUEUE_TRANS_EMPTY                    -3
-#define RET_QUEUE_TRANS_TIMEOUT                  -4
+#define RET_QUEUE_TRANS_INVALID -1
+#define RET_QUEUE_TRANS_FULL -2
+#define RET_QUEUE_TRANS_EMPTY -3
+#define RET_QUEUE_TRANS_TIMEOUT -4
 
 // types
 
@@ -41,15 +41,17 @@ typedef void (*task_func_t)(void *);
 
 int32_t os_start(void);
 
-int32_t task_create(task_func_t entry, void *arg, uint32_t stack_size, uint8_t priority);
-int32_t task_create_isr(task_func_t entry, void *arg, uint32_t stack_size, uint8_t priority);
+int32_t task_create(task_func_t entry, void *arg, uint32_t stack_size,
+                    uint8_t priority);
+int32_t task_create_isr(task_func_t entry, void *arg, uint32_t stack_size,
+                        uint8_t priority);
 
-void    task_sleep(uint32_t ms);
+void task_sleep(uint32_t ms);
 
-void    task_exit(void);
+void task_exit(void);
 
-void    task_kill(uint16_t task_id);
-void    task_kill_isr(uint16_t);
+void task_kill(uint16_t task_id);
+void task_kill_isr(uint16_t);
 
 int32_t queue_create(uint32_t item_size, uint32_t length);
 int32_t queue_create_isr(uint32_t item_size, uint32_t length);
@@ -60,8 +62,8 @@ int32_t queue_push_isr(uint16_t queue_id, void *item);
 int32_t queue_pull(uint16_t queue_id, void *item, uint32_t timeout);
 int32_t queue_pull_isr(uint16_t queue_id, void *item);
 
-void    queue_close(uint16_t queue_id);
-void    queue_close_isr(uint16_t queue_id);
+void queue_close(uint16_t queue_id);
+void queue_close_isr(uint16_t queue_id);
 
 void enter_critical(void);
 void leave_critical(void);
